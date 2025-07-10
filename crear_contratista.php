6902e9deb0f49b0be929251aa8cbf5521327d1e8
helimir <helimirlopez@gmail.com>
2025-07-10 10:56:40 -0400

    modificacion crear_contratista

--- crear_contratista.php
+++ crear_contratista.php
@@ -1226,7 +1226,7 @@ function validar_rep(rut_rep) {
                                                                                               var valores=$('#frmContratistas').serialize();
                                                                                               $.ajax({
                                                                                     			method: "POST",
-                                                                                                url: "add/contratistas.php",
+                                                                                                url: "add/add_contratistas.php",
                                                                                                 data: valores,
                                                                                                 beforeSend: function(){
                                                                                                     $('#modal_cargar').modal('show');						
